bad068b38f517f1d80bee1700de9d64782285777
damianlips <damianlips87@gmail.com>
2022-04-17 15:34:57 -0300

    Agrega metodos a implementar

--- src/etapa_dos/PlantEnvironment.java
+++ src/etapa_dos/PlantEnvironment.java
@@ -6,5 +6,29 @@ import frsf.cidisi.faia.agent.Agent;
 import frsf.cidisi.faia.agent.Perception;
 
 public class PlantEnvironment extends Environment {
+	public PlantEnvironment() {
+		this.environmentState = new PlantEnvironmentState();
+	}
+
+	@Override
+	public Perception getPercept() {
+		// TODO Auto-generated method stub
+		PlantPerception perception = new PlantPerception();
+		return null;
+	}
+	
+	@Override
+    public PlantEnvironmentState getEnvironmentState() {
+        return (PlantEnvironmentState) super.getEnvironmentState();
+    }
+	@Override
+    public String toString() {
+        return environmentState.toString();
+    }
+	@Override
+    public boolean agentFailed(Action actionReturned) {
+		return false;
+	}
+	 
 
 }
